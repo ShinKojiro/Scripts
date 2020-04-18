f93828d0b16fe37c26160803d7bb41130323d5fd
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-17 23:01:28 -0500

    Continuing

---
+++ DataTypeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypeScript : MonoBehaviour
+{
+    private void Start()
+    {
+        /*Vector3 currentPosition = transform.position;
+        currentPosition = new Vector3(0, 2, 0);*/
+
+        Transform tran = transform;
+        tran.position = new Vector3(0, 3, 0);
+    }
+}
