7f13a94b64744951ad7cac74014e2b2ab9211627
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-18 11:44:34 -0500

    again, fun

--- CardinalDirection.cs
+++ CardinalDirection.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CardinalDirection : MonoBehaviour
 {
+    enum Direction {North, East, South, West};
     // Start is called before the first frame update
     void Start()
     {
-        
+        Direction myDirection;
+
+        myDirection = Direction.North;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Direction ReverseDirection(Direction dir)
+    {
+        if (dir == Direction.North)
+        {
+            dir = Direction.South;
+        }
+        else if (dir == Direction.South)
+        {
+            dir = Direction.North;
+        }
+        else if (dir == Direction.East)
+        {
+            dir = Direction.West;
+        }
+        else if (dir == Direction.West)
+        {
+            dir = Direction.East;
+        }
+
+        return dir;
     }
 }
--- InvokeRepeating.cs
+++ InvokeRepeating.cs
@@ -10,19 +10,21 @@ public class InvokeRepeating : MonoBehaviour
     {
         InvokeRepeating("SpawnObject", 2, 1);
 
-        CancelInvoke("SpawnObject");
+        //CancelInvoke("SpawnObject");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Random.Range(-20.0f, 20.0f);
+        /*float x = Random.Range(-20.0f, 20.0f);
         float z = Random.Range(-20.0f, 20.0f);
-        Instantiate(target, new Vector3(x, 10, z), Quaternion.identity);
+        Instantiate(target, new Vector3(x, 10, z), Quaternion.identity);*/
     }
 
     void SpawnObject()
     {
-        
+        float x = Random.Range(-20.0f, 20.0f);
+        float z = Random.Range(-20.0f, 20.0f);
+        Instantiate(target, new Vector3(x, 10, z), Quaternion.identity);
     }    
 }
