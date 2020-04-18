880b38b8cc849314c007ecd876cc1a7281128fe7
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-18 11:12:29 -0500

    Yet more fun.

---
+++ InvokeRepeating.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeating : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+
+        CancelInvoke("SpawnObject");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Random.Range(-20.0f, 20.0f);
+        float z = Random.Range(-20.0f, 20.0f);
+        Instantiate(target, new Vector3(x, 10, z), Quaternion.identity);
+    }
+
+    void SpawnObject()
+    {
+        
+    }    
+}
