9d8ff26fc4a2ea9e8e90da81870161023eb17ce8
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-18 02:44:52 -0500

    Fun stuff

---
+++ Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int projectileA;
+        public int projectileB;
+        public int projectileC;
+        public float fuel;
+
+        public Stuff(int prA, int prB, int prC)
+        {
+            projectileA = prA;
+            projectileB = prB;
+            projectileC = prC;
+        }
+
+        public Stuff(int prA, float fu)
+        {
+            projectileA = prA;
+            fuel = fu;
+        }
+
+        //Constructor
+        public Stuff()
+        {
+            projectileA = 1;
+            projectileB = 1;
+            projectileC = 1;
+        }
+    }
+
+    // Creating an Instance (an Object) of the Stuff class
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+
+    public Stuff myImportantStuff = new Stuff();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log(myStuff.projectileA);
+
+        // mutators addded on my own knowledge.
+        myImportantStuff.projectileA = 3;
+        myImportantStuff.projectileB = 2;
+        myImportantStuff.projectileC = 50;
+        myImportantStuff.fuel = 200.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UsingInstantiate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingInstantiate : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform cylinderEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectile, cylinderEnd.position, cylinderEnd.rotation) as Rigidbody;
+            projectileInstance.AddForce(cylinderEnd.up * 350f);
+        }
+    }
+}
